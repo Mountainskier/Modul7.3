4c2a04211c46017054557ccdfe4413e019ec1522
Владимир <169135025+Mountainskier@users.noreply.github.com>
2024-07-05 23:06:59 +0300

    Modul7.3.py

--- Modul7.3.py
+++ Modul7.3.py
@@ -37,5 +37,5 @@ class WordsFinder:
 
 finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt')
 print(finder1.get_all_words())
-print(finder1.find('captain'))
-print(finder1.count('captain'))
+print(finder1.find('capTAIN'))
+print(finder1.count('CAptaIN'))
